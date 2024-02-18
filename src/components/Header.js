bcf9867c2b78ac03cd3601aa784bcde11f651d12
Alejandro Dueñas <luisdue3@gmail.com>
2024-02-18 12:30:17 -0500

    Components

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+// 29:10
+
+const Header = ({ title }) => {
+	return (
+		<header>
+			<h1>{title}</h1>
+		</header>
+	)
+}
+
+Header.defaultProps = {
+	title: 'Task Tracker'
+}
+
+export default Header
